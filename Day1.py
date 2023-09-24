e30bab041d8b93f378b15614c07ae5484af20514
Gabkoi <gabakoi18@gmail.com>
2023-09-24 14:38:30 +0000

    This  is my Angella project

---
+++ Day1.py
@@ -0,0 +1,87 @@
+# First Excercise
+"""
+print("Day 1 - Python print Function")
+print("The function is declared like this:")
+print("print('What to print')")
+"""
+
+
+# String Manipulstion and Concatenation 
+"""
+print("Hello world \nHello world\nHello world")
+
+# string Concatenation
+print("Hello" + " " + "Angela")
+"""
+
+# second Excercise
+'''
+print("Day 1 - String Manunolation")
+print("String Concatenation is done with the '+' sign")
+print("e.g. print('Hello' + 'world')")
+'''
+
+
+# python input Function
+'''
+# input("What is your name?")
+# input() will get user input in the console
+# Then print() will print the  word "Hello" and user input
+
+print("Hello " + input(' What is your name ') + "!")
+'''
+
+# input Function Exercise
+'''
+# In this program you are allow to the  user for their
+# name and when they in put in their name they will 
+# see the  len or number of their name
+ 
+print(len(input("what is your name? ") ) )
+'''
+
+# Python Variable 
+''''
+name = input("What is your name?")
+print(name) 
+
+name = "Jack"
+print(name)
+
+name = input("What is your name")
+length = len(name)
+print(length)
+
+name = "Greek for Greek"
+print(name)
+
+# Variable EXercise
+a = 10
+b = 3
+c = b
+b = a
+a = c
+print(b)
+'''
+
+
+# Day1 Final Project
+# this project is a band name  Generator project. we the purpose of this project is that it 
+# prompt the user to input their city name and as well as their favority pet
+# and than joined the both name.
+
+
+print("welcome to the band naming generator")
+
+city_Name = input("What is the name of your city you grew up in \n")
+
+pet_Name = input("What is the name of your pet \n")
+
+print("Your band name could be " + ( city_Name + " " + pet_Name))
+
+
+ 
+ 
+
+ 
+
---
+++ Day2.py
@@ -0,0 +1,100 @@
+# Data Types
+
+# String
+'''
+print("Hello"[4])
+
+print("123" + "234")
+'''
+
+# Interger
+'''
+print(12+2)
+print (32)
+'''
+
+# float
+'''
+print(34.56)
+'''
+
+
+#  name convertion
+'''
+work = "Gabriel"
+print(type(work))
+
+a = float(123)
+print(type(a))
+'''
+
+
+# Exercise day2  
+'''
+num1 = int(input())
+num2 = int (input())
+print(num1 + num2)
+'''
+
+# Math Operator 
+'''print(3*3+3/3-3)'''
+
+# Body weight Exercise
+'''
+weight = int(input("Enter your body weight "))
+height = float(input("Enter your body height "))
+BMI = weight / height **2 
+print(int(BMI))
+'''
+
+
+# rounding number in python with the round Function
+'''
+print(round(3/5))
+
+result = 4/2
+result /= 2
+print(round(result))
+
+score = 0
+score += 1
+print(round(score))
+'''
+
+# Python F-String
+'''
+score = 0
+hieght = 1.4
+print(f"This is your score {score} \n This your player hieght is {hieght}")
+'''
+
+
+# F-string Exercise of Day2 
+
+'''
+age = int(input("What is your current Age "))
+
+years = 90 - age
+days = years * 365
+weeks = years * 52
+months = years * 12
+
+message = f"you have {days} days, you have {weeks} weeks, you have {months} month left"
+print(message)
+'''
+
+
+# Final project (Tip Caclulator)
+
+print("Welcome to the tip calculator ")
+total_bill = float(input("What is the total bill "))
+tip_percentage = float(input("What percentage would like to give "))
+user_name = int(input("How manay user can split the money"))
+bill_tip = tip_percentage / 100 * total_bill + total_bill
+bill_per_person = total_bill / user_name
+final_amount  = round(bill_per_person)
+print(f"Each person should payed {final_amount}")
+
+
+
+
