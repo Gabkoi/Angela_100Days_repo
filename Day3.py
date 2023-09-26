1f9f599eb0c627b4a4d3d54fa1d8c972ca3768da
Gabkoi <gabakoi18@gmail.com>
2023-09-26 16:50:25 +0000

    python 100days

--- Day3.py
+++ Day3.py
@@ -12,28 +12,40 @@ else:
 """
 
 
-# coster rider  >= greater or equal to operator
+# rollercoaster  rider  >= greater or equal to operator
+'''
+hieght = int(input("what is your hieght in cm "))
 
-"""
-rider_hieght = int(input("what is your hieght "))
-
-if rider_hieght >= 120:
-    print("You can ride the rollercoaster ")
+if hieght >= 120:
+    print("You can ride the rollercoaster")
     
-    age = int(input("What is tour age "))
-
+    bill = 0
+    age = int(input("What is your age "))
     if age < 12:
-        print("You are allwo to used the $5 Ticket")
+        bill = 5
+        print("Child ticket are $5")
 
     elif age <= 18:
-        print(" you are allow to buy the $7 ticket")
-      
+        bill = 7
+        print("Youth ticket are $7")
+
     else:
-      print("You are allow you are allow to the $12 ticket")
-      
+        bill = 12
+        print("Adult Ticket are $12")
+
+    want_photos = input("Do you want a photos taken? Y or N  \n")
+
+    if want_photos == "Y":
+        bill += 3
+        print(f"Your final bill is ${bill}")
+
+    elif want_photos == "N":
+        print(f"You are still allow to payed {bill}")
+
 else:
-    print("You are not allow to ride the rollercoaster ")
-    """
+    print("Sorry, yo hsve to grow taller before you can ride rollercoster")
+'''
+
 
 
 # play groud != less than or equal to operator
@@ -87,7 +99,6 @@ else:
 '''
 
 
-
 # Leap Year Excercise using the  if else
 '''
 year = int(input("Enter your year you want "))
@@ -102,3 +113,20 @@ else:
 '''   
 
 
+# Program for my PIzza Cafe 
+
+print("Welcome to the  pizza Diliveries ")
+
+size = input("What size do you want? S, M, or L ")
+
+add_pepperoni = input("Do you have pepperoni? Y or N ")
+
+extra_cheese = input("Do you want a extra chase? Y or N")
+
+
+
+
+
+# import urllib.request
+
+# webUrl = urllib.request.urlopen("https://www.javatpoint.com/python-tutorial")
